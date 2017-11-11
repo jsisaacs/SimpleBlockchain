7850afbd5dde0faa1fccea5de0dc17ea424e1f35
Josh Isaacson <jsisaacs@iu.edu>
2017-11-11 02:34:20 -0500

    added calculateHash and calculateHashForBlock

--- Blockchain/src/Block.java
+++ Blockchain/src/Block.java
@@ -1,4 +1,8 @@
+import java.nio.charset.StandardCharsets;
+import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.security.*;
+import java.util.Arrays;
 
 public class Block {
     private int index;
@@ -18,4 +22,17 @@ public class Block {
     public static Block getGenesisBlock() {
         return new Block(0, "0", 0, "Genesis block data", "hash");
     }
+
+    public String calculateHash(int index, String previousHash, long timeStamp, String data) throws NoSuchAlgorithmException {
+        String value = Integer.toString(index) + previousHash + Long.toString(timeStamp) + data;
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(value.getBytes(StandardCharsets.UTF_8));
+        return Arrays.toString(hash);
+    }
+
+    public String calculateHashForBlock(Block b) throws NoSuchAlgorithmException {
+        return calculateHash(b.index, b.previousHash, b.timeStamp, b.data);
+    }
+
+
 }
--- Blockchain/src/Client.java
+++ Blockchain/src/Client.java
@@ -1,7 +1,8 @@
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         ArrayList<Block> blockchain = new ArrayList<>();
         blockchain.add(Block.getGenesisBlock());
     }
